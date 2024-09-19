438136a133269ab7eeb04ba79be784169f8023a5
hammemi nour allah <hammemi.nourallah@gmail.com>
2024-09-19 19:46:31 +0200

    new web cv

---
+++ index.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('section');
+
+    const revealSection = (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+            }
+        });
+    };
+
+    const sectionObserver = new IntersectionObserver(revealSection, {
+        threshold: 0.1
+    });
+
+    sections.forEach(section => {
+        sectionObserver.observe(section);
+    });
+});
