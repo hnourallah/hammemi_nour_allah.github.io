8907bd79fb42f5059c38c9fed1fd94fd7346e9dd
hammemi nour allah <hammemi.nourallah@gmail.com>
2024-09-21 22:30:14 +0200

    desing 2

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const navLinks = document.querySelectorAll('.navbar ul li a');
     const sections = document.querySelectorAll('.section');
 
-    // Fonction pour masquer toutes les sections
     function hideAllSections() {
         sections.forEach(section => {
             section.classList.remove('active');
         });
     }
 
-    // Fonction pour afficher une section
     function showSection(sectionId) {
         hideAllSections();
         const section = document.getElementById(sectionId);
@@ -18,10 +16,8 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    // Affiche la section "about" par défaut
     showSection('about');
 
-    // Gestion des clics
     navLinks.forEach(link => {
         link.addEventListener('click', function(event) {
             event.preventDefault();
