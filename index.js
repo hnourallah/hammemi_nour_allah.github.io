fe37f0c50f661833761cef11f982d3731b394f0b
hammemi nour allah <hammemi.nourallah@gmail.com>
2024-09-25 22:38:55 +0200

    3.3

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sections = document.querySelectorAll('.section');
     const discoverBtn = document.getElementById('discover-btn');
+    const card = document.querySelector('.card');
 
     // Fonction pour cacher toutes les sections
-    //function hideAllSections() {
-        //sections.forEach(section => {
-            //section.classList.remove('active');
-      //  });
-    //}
+    function hideAllSections() {
+        sections.forEach(section => {
+            section.classList.remove('active');
+        });
+    }
 
     // Écouteur d'événements pour le bouton "Découvrir mes compétences"
     discoverBtn.addEventListener('click', function () {
         hideAllSections();
-        document.getElementById('skills').classList.add('active'); // Affiche la section compétences
+
+        // Transition de la carte
+        card.style.transition = 'transform 0.5s ease';
+        card.style.transform = 'scale(0.1)'; // Réduit la carte
+
+        setTimeout(() => {
+            card.style.transform = 'scale(1)'; // Restauration de la carte
+
+            document.getElementById('skills').classList.add('active'); // Affiche la section compétences
+        }, 500); // Délai correspondant à la durée de l'animation
     });
 
     // Affiche la section "À propos" par défaut au chargement
